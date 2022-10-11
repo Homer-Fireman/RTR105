3da0b708236ecfd0ea3fe958d4e23c81a478b91c
Homer-Fireman <markusssube9@gmail.com>
2022-10-11 09:59:37 +0300

    scripta_izmainas()

---
+++ C/PU2_PT2/PU2:PT2.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+int main(void){
+    unsigned char ui, result, c;
+
+    printf("Please input 1 natuar number ");
+    scanf("%c", &ui);
+
+    
+    printf("in binary it is ");
+    for (c = 31; c >= 0; c--)
+  {
+    result = ui >> c;
+
+    if (result & 1)
+      printf("1");
+    else
+      printf("0");
+    printf("\n");
+    return 0;
+  }
+}
---
+++ C/class9/char_data_type.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+
+int main(void){
+
+    char a =10;
+    
+
+
+
+    return 0;
+}
