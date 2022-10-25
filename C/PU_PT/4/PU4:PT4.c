e248c9b005a17b02e6a6ee42240a65a03f6bb523
Homer-Fireman <markusssube9@gmail.com>
2022-10-25 09:56:19 +0300

    scripta_izmainas()

---
+++ C/PU_PT/4/PU4:PT4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+
+    char varable[5];
+    int dec,i;
+    int res=1;
+
+    printf("Input one decimal number\n");
+    scanf("%d",&dec);
+   // printf("Input varable (char, int or long long)\n");
+   // scanf("%s",varable);
+
+   if(dec>0){
+    for(i=1;i<=dec;i++){
+        res=res*i;
+    }
+   }else{
+    for(i=1;i<=-dec;i++){
+        res=res*i;
+    }
+   }
+
+    
+
+    
+
+    if(dec>0){
+        printf("Factorial of %d is %d\n",dec,res);
+    }else{
+        printf("Factorial of %d is -%d\n",dec,res);
+    }
+
+    return 0;
+}
